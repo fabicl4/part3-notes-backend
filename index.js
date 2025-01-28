3d8d84ff77d5ac45da81f4fb22f37a363904d0b5
fabic <fcondelafuente@gmail.com>
2025-01-28 11:41:03 +0100

    update binding port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 let notes = [
     {
@@ -83,7 +84,6 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
   })
 
-const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${port}`)
 })
